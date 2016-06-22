d3bece948171af6cc0f959dde2fb1f96d0b46bc0
Marcin Kozlowski <marcinguy@gmail.com>
2016-06-22 14:20:32 +0200

    Initial commit

---
+++ embparpbar.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" A multiprocess `Pool` variant with a `ProgressBar`.  """
+
+__author__ = "Valentin Haenel <valentin.haenel@epfl.ch>"
+
+import time
+from multiprocessing.pool import Pool
+from progressbar import ProgressBar, Percentage, Bar, ETA
+
+class ProgressPool(Pool):
+    """ Extension of `multiprocessing.Pool` with `ProgressBar`.
+
+    The `map` function now displays a progress bar. The usual caveats about
+    multiprocessing not working in an interactive interpreter apply.
+
+    """
+    def add_pgbar(self, bar):
+        self.bar = bar
+    def add_loop(self, loop):
+        self.loop = loop
+
+
+    def map(self, func, iterable, chunksize=1, pbar='ProgressPool'):
+        """ Apply function on iterables in available subprocess workers.
+
+        Parameters
+        ----------
+        func : callable
+            the function to execute
+        iterable : iterable
+            the arguments to the func
+        chunksize : int, default: 1
+            the approximate number of tasks to distribute to a process at once
+        pbar : str or ProgressBar
+            if str, use the string in a standard ProgressBar, else use the
+            given ProgressBar
+
+        Returns
+        -------
+        results : list
+            the result of applying func to each value in iterables
+
+        Raises
+        ------
+        TypeError if the pbar argument has the wrong type
+
+        """
+        # need to get the length, for the progress bar
+        if not hasattr(iterable, '__len__'):
+            iterable = list(iterable)
+        total_items = len(iterable)
+
+        # initialize the progress bar
+        self.bar.done = total_items
+
+        # get the pool working asynchronously
+        a_map = self.map_async(func, iterable, chunksize)
+
+        # Crux: monitor the _number_left of the a_map, and update the progress
+        # bar accordingly
+        # TODO should probably check for termination on each run here
+        while True:
+            time.sleep(0.1)
+            left = a_map._number_left
+            self.bar.set_completion(total_items-left)
+
+            if left == 0:
+                break
+            self.loop.draw_screen()
+        
+        return a_map.get()
+
---
+++ pscanner.py
@@ -0,0 +1,263 @@
+#!/usr/bin/python
+
+import sys
+import urwid
+import os
+
+import argparse
+
+import multiprocessing
+
+import ssl
+import socket
+from socket import gethostbyname, gaierror
+
+from dns import reversename, resolver, exception
+import sys
+
+import M2Crypto
+from embparpbar import ProgressPool
+
+
+import pprint
+
+
+def scan_nossl(d):
+          d=str(d)
+          try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.1)
+            result = s.connect_ex((d, int(port)))
+            s.close()
+          except Exception, e:
+            message = "Error: "+d.rstrip()+","+getrev(d)
+            message += str(e)
+            return message
+          if result:
+            return d.rstrip()+","+port+",closed"
+          else:
+            return d.rstrip()+","+port+",open"
+          
+
+def scan_ssl(d):
+          s_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          s = ssl.wrap_socket(s_, ca_certs='/usr/local/lib/python2.7/dist-packages/requests/cacert.pem',cert_reqs=ssl.CERT_OPTIONAL)
+
+          s.settimeout(0.1)
+          d=str(d)
+          try:
+            result = s.connect_ex((d, int(port)))
+          except Exception, e:
+                message = "Error: "+d.rstrip()+","+getrev(d)
+                message += str(e)
+                try:
+                  cert = ssl.get_server_certificate((d, 443), ssl_version=ssl.PROTOCOL_TLSv1)
+                  x509 = M2Crypto.X509.load_cert_string(cert)
+                  r = x509.get_subject().as_text()
+                  val = r.split(",")
+                  for i, j in enumerate(val):
+                    if j.find("CN=") != -1:
+                      val[i]=j.replace("CN=","")
+                      val[i]=val[i].strip()
+                  message += ","+val[i]
+                  return message
+                except Exception, e:
+                       return d.rstrip()+","+getrev(d)+","+"CERT ERROR!"
+          except ssl.SSLError:
+            return d.rstrip()+","+getrev(d)+","+"CERT ERROR!"
+          except socket.gaierror:
+            return d.rstrip()+","+"SOCKET ERROR!"
+          except socket.error:
+            return d.rstrip()+","+"SOCKET ERROR!"
+          if result:
+            return "No SSL"
+          else:
+            cert = s.getpeercert()
+            if cert:
+              subject = dict(x[0] for x in cert['subject'])
+              issued_to = subject['commonName']
+            else:
+              subject = "None"
+              issued_to = "None"
+            return d.rstrip()+","+getrev(d)+","+"CN:"+issued_to+","+"CERT OK"
+
+
+
+
+
+def getrev(ip):
+
+      try:
+        rev_name = reversename.from_address(str(ip.rstrip()))
+        resolverobj = resolver.Resolver()
+        resolverobj.timeout = 1
+        resolverobj.lifetime = 1
+        reversed_dns = str(resolverobj.query(rev_name,"PTR")[0])
+        reversed_dns = reversed_dns[:-1]
+        return reversed_dns
+      except resolver.NXDOMAIN:
+        return "unknown"
+      except resolver.Timeout:
+        return "Timed out while resolving %s" % ip.rstrip()
+      except exception.DNSException:
+        return "Unhandled exception"
+
+
+
+class DialogExit(Exception):
+    pass
+
+
+class DialogDisplay:
+    palette = [
+        ('body','black','light gray', 'standout'),
+        ('border','black','dark blue'),
+        ('shadow','white','black'),
+        ('selectable','black', 'dark cyan'),
+        ('focus','white','dark blue','bold'),
+        ('focustext','light gray','dark blue'),
+        ]
+    
+
+    def __init__(self, text, height, width, body=None):
+        self.bar1 = urwid.ProgressBar('pg normal', 'pg complete')
+        width = int(width)
+        if width <= 0:
+            width = ('relative', 80)
+        height = int(height)
+        if height <= 0:
+            height = ('relative', 80)
+
+        self.body = body
+        if body is None:
+            # fill space with nothing
+            body = urwid.Filler(urwid.Divider(),'top')
+
+        self.frame = urwid.Frame( body, focus_part='footer')
+        if text is not None:
+            self.frame.header = urwid.Pile( [urwid.Text(text),
+                urwid.Divider(), self.bar1] )
+
+
+        # Adding progressbar
+
+        #self.frame = urwid.Frame(urwid.ListBox([bar1]))
+
+        w = self.frame
+
+        # pad area around listbox
+        w = urwid.Padding(w, ('fixed left',2), ('fixed right',2))
+        w = urwid.Filler(w, ('fixed top',1), ('fixed bottom',1))
+        w = urwid.AttrWrap(w, 'body')
+
+        # "shadow" effect
+        w = urwid.Columns( [w,('fixed', 2, urwid.AttrWrap(
+            urwid.Filler(urwid.Text(('border','  ')), "top")
+            ,'shadow'))])
+        w = urwid.Frame( w, footer =
+            urwid.AttrWrap(urwid.Text(('border','  ')),'shadow'))
+
+        # outermost border area
+        w = urwid.Padding(w, 'center', width )
+        w = urwid.Filler(w, 'middle', height )
+        w = urwid.AttrWrap( w, 'border' )
+
+        self.view = w
+
+
+    def add_buttons(self, buttons):
+        l = []
+        for name, exitcode in buttons:
+            if name=="Stop":
+              b = urwid.Button( name, self.button_press_stop )
+              b.exitcode = exitcode
+            if name=="Start":
+              b = urwid.Button( name, self.button_press_start )
+              b.exitcode = exitcode
+            b = urwid.AttrWrap( b, 'selectable','focus' )
+            l.append( b )
+        self.buttons = urwid.GridFlow(l, 10, 3, 1, 'center')
+        self.frame.footer = urwid.Pile( [ urwid.Divider(),
+            self.buttons ], focus_item = 1)
+
+    def button_press_stop(self, button):
+        raise DialogExit(button.exitcode)
+
+
+    def button_press_start(self, button):
+        self.do_scan()
+
+
+    def dosomething(self, steps):
+        self.bar1.done = steps
+        for i in range(steps + 1):
+          self.bar1.set_completion(i)
+          os.system('sleep 0.2')
+          self.loop.draw_screen()
+
+
+
+    def do_scan(self):
+       global port
+
+       parser = argparse.ArgumentParser(description='Port Scanner v0.99')
+       parser.add_argument('-i','--input', help='Input list of IPs', required=True)
+       parser.add_argument('-o','--output', help='Output', required=True)
+       parser.add_argument('-p','--port', help='Port number(s)', required=True)
+       parser.add_argument('-s','--ssl', help='SSL yes|no', required=True)
+       args = parser.parse_args()
+       input = args.input
+       output = args.output
+       port = args.port
+       sslp = args.ssl
+       data = open(input,'r')
+       # Create pool (ppool)
+       ppool = ProgressPool()
+
+       ppool.add_pgbar(self.bar1)
+       ppool.add_loop(self.loop)
+       if(sslp=="yes"):
+         results = ppool.map(scan_ssl, data)
+       else:
+         results = ppool.map(scan_nossl, data)
+       #pprint.pprint(results)
+       resfile = open(output,'w')
+       for r in results:
+         resfile.write(r+"\n")
+
+
+
+    def main(self):
+
+        self.loop = urwid.MainLoop(self.view, self.palette)
+        try:
+
+            self.loop.run()
+        except DialogExit, e:
+            return self.on_exit( e.args[0] )
+
+    def on_exit(self, exitcode):
+        return exitcode, ""
+
+
+
+
+def main():
+
+
+    d = DialogDisplay( "text", 50, 50)
+    d.add_buttons([    ("Stop", 0), ("Start", 0) ])
+
+
+    # Run it
+    exitcode, exitstring = d.main()
+
+    # Exit
+    if exitstring:
+        sys.stderr.write(exitstring+"\n")
+
+    sys.exit(exitcode)
+
+
+if __name__=="__main__":
+    main()
